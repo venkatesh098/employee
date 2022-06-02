80d0a26c982326c86c53be615dc3ecf12947e22a
venkat <kormanivenkatesh098@gmail.com>
2022-06-03 03:02:24 +0530

    dev-bug-14

--- src/main/java/com/maven/bankingapplication/BankingApplication.java
+++ src/main/java/com/maven/bankingapplication/BankingApplication.java
@@ -5,6 +5,7 @@ public class BankingApplication {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("dev-bug-12");
+		System.out.println("user written code and trying to push the code to master branch without pulling the code dev-bug-14");
 
 	}
 
